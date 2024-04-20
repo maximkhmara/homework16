4309fc92c34ce7b1b9ae0c86dc42aecaa00a4a3f
Max <maxcatalina@iMac-Max.local>
2024-04-20 11:15:01 +0300

    w

--- main.js
+++ main.js
@@ -1,97 +1,59 @@
-const shopProduct = document.querySelector('.shop-product');
-const buyBtn = document.querySelector('.shop-product-buy');
-const buyForm = document.querySelector('.buy-form');
-const formFields = document.querySelectorAll('.form-field');
-const order = document.querySelector('.order');
-const orderContent = document.querySelector('.order-content');
-const formSubmitBtn = document.querySelector('.form-submit-btn');
+function Human(name, gender) {
+	this.name = name;
+	this.gender = gender;
 
-buyBtn.addEventListener('click', () => {
-	buyForm.classList.add('show');
-});
-
-const userNameInput = document.querySelector('.user-name-check');
-const userCityInput = document.querySelector('.user-phone-check');
-const userPostInput = document.querySelector('.user-email-check');
-const userNameRegex = /^[A-Z][a-z]+\s[A-Z][a-z]+\s[A-Z][a-z]+$/;
-const userPhoneRegex = /^\+?[0-9()-]{10,13}$/;
-const userEmailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-let nameIsValid = false;
-let phoneIsValid = false;
-let emailIsValid = false;
-
-function validateField(event, regex, errorClass) {
-	const isValid = regex.test(event.target.value);
-	const errorElement = document.querySelector(errorClass);
-
-	if (isValid) {
-		errorElement.style.display = 'none';
-	} else {
-		errorElement.style.display = 'block';
-	}
-
-	return isValid;
+	this.intro = function () {
+		console.log(`Name: ${this.name} Gender: ${this.gender}.`);
+	};
 }
 
-userNameInput.addEventListener('input', (event) => {
-	nameIsValid = validateField(
-		event,
-		userNameRegex,
-		'.name-error-text',
-		nameIsValid
-	);
-	checkFormValidity();
-});
+function Flat() {
+	this.residents = [];
 
-userCityInput.addEventListener('input', (event) => {
-	phoneIsValid = validateField(
-		event,
-		userPhoneRegex,
-		'.phone-error-text',
-		phoneIsValid
-	);
-	checkFormValidity();
-});
-
-userPostInput.addEventListener('input', (event) => {
-	emailIsValid = validateField(
-		event,
-		userEmailRegex,
-		'.email-error-text',
-		emailIsValid
-	);
-	checkFormValidity();
-});
-
-function checkFormValidity() {
-	if (nameIsValid && phoneIsValid && emailIsValid) {
-		formSubmitBtn.classList.remove('disable');
-	} else {
-		formSubmitBtn.classList.add('disable');
-	}
+	this.addResident = function (human) {
+		if (human instanceof Human) {
+			this.residents.push(human);
+			return this.residents;
+		} else {
+			return 'Error';
+		}
+	};
 }
 
-buyForm.addEventListener('submit', function (event) {
-	event.preventDefault();
-	shopProduct.classList.add('hide');
-	buyForm.classList.remove('show');
-	order.classList.add('show');
-	const formData = new FormData(this);
-	const formObject = {};
+function Building(maxFlats) {
+	this.flats = [];
+	this.maxFlats = maxFlats;
 
-	formData.forEach((value, key) => {
-		formObject[key] = value;
-	});
+	this.addFlat = function (flat) {
+		if (this.flats.length < this.maxFlats) {
+			this.flats.push(flat);
+		} else {
+			console.log('Cannot add new flat. Maximum number of flats reached.');
+		}
+	};
+}
 
-	const orderName = document.createElement('div');
-	const orderPhone = document.createElement('div');
-	const orderEmail = document.createElement('div');
+const person1 = new Human('John', 'male');
+const person2 = new Human('Mike', 'male');
+const person3 = new Human('Max', 'male');
+const person4 = new Human('Irena', 'female');
 
-	orderName.textContent = `Ваше ім'я: ${formObject.username}`;
-	orderPhone.textContent = `Ваш телефон: ${formObject.phone}`;
-	orderEmail.textContent = `Ваш email: ${formObject.email}`;
+const flat1 = new Flat();
+const flat2 = new Flat();
+flat1.addResident(person1);
+flat1.addResident(person2);
+flat2.addResident(person3);
+flat2.addResident(person4);
 
-	[orderName, orderPhone, orderEmail].forEach((element) => {
-		orderContent.appendChild(element);
+const building1 = new Building(3);
+console.log(`Maximum number of flats in the building: ${building1.maxFlats}`);
+building1.addFlat(flat1);
+building1.addFlat(flat2);
+
+building1.flats.forEach((el) => {
+	el.residents.forEach((person) => {
+		console.log(`Residents of the building: ${person.name}`);
 	});
 });
+
+console.log(building1.flats);
